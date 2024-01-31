8b7520ff3a20bd04921a6d14764fa11ccbd3c372
jack4me <just4go@gmail.com>
2024-01-31 12:53:00 +0100

    +3

--- Assets/CodeBase/UI/Elements/HpBar.cs
+++ Assets/CodeBase/UI/Elements/HpBar.cs
@@ -5,8 +5,8 @@ namespace UI.Elements {
     public class HpBar : MonoBehaviour {
         public Image imageCurrent;
 
-        public void SetValue(float Current, float Max){
-            imageCurrent.fillAmount = Current / Max;
+        public void SetValue(float current, float max){
+            imageCurrent.fillAmount = current / max;
         }
     }
 }
